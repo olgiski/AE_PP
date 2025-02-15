b21866d79c1f290427e79683957f30a1428c4ba4
olgiski <zarazka2082@gmail.com>
2025-02-15 17:57:10 -0600

    Home Page Added and set up

---
+++ pw_setup/automation_exercise/tests/test_home_page.py
@@ -0,0 +1,9 @@
+import pytest
+from pages.home_page import HomePage
+from utils import config
+
+def test_home_page(page):
+    homepage = HomePage(page)
+    homepage.visit(config.BASE_URL)
+    homepage.validate_title("Automation Exercise")
+    homepage.signup_login_button
