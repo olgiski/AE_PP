69416dae5f935c5157d04023ab81f8dadb3648e2
olgiski <zarazka2082@gmail.com>
2025-02-15 18:10:00 -0600

    added import Page

--- pw_setup/automation_exercise/pages/enter_account_info_page.py
+++ pw_setup/automation_exercise/pages/enter_account_info_page.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import Playwright
 from pages.base_page import BasePage
+from playwright.sync_api import Page
+
 
 class EnterAccountInfo:
     def __init__(self, page:Page):
--- pw_setup/automation_exercise/pages/signup_login_page.py
+++ pw_setup/automation_exercise/pages/signup_login_page.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import Playwright
 from pages.base_page import BasePage
+from playwright.sync_api import Page
+
 
 class SignupLoginPage:
     def __init__(self,page: Page):
@@ -8,4 +10,3 @@ class SignupLoginPage:
         self.new_user_name_input = "//input[@name = 'name']"
         self.new_user_email_input = "//input[@data-qa= 'signup-email']"
         self.signup_button = "//button[contains(text(),'Signup')]"
-        
