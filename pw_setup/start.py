2385ae3f47c9e3aebbbed3b6c3bbf68fd7115428
olgiski <zarazka2082@gmail.com>
2025-02-15 18:05:33 -0600

    Updated start.py

--- pw_setup/start.py
+++ pw_setup/start.py
@@ -2,8 +2,10 @@ from playwright.sync_api import sync_playwright
 
 def test_google():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto("https://www.google.com")
-        assert "Google" in page.title()
+        page.goto("https://www.automationexercise.com/")
+        print(page.title)
+        title = "Automation Exercise"
+        assert title in page.title()
         browser.close()
